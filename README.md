110b2c9fb95340fd1b7c93df781b2eb03759d7dd
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2022-10-08 03:36:45 +0600

    test with flaker

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DjangoAdvance
+
+docker commands:<br>
+    docker build .<br>
+    docker-compose build<br>
+    docker-compose run app sh -c "python manage.py test && flake8"<br>
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from app.calc import add
 
+
 class CalcTests(TestCase):
-    def test_add_numbers(self):
+    def test_add_numbers_pass(self):
         """Test that two numbers are added together"""
-        self.assertEqual(add(3,8),10)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_add_numbers_fail(self):
+        """Test that two numbers are added together"""
+        # self.assertEqual(add(3,8),10)                 # to make it fail
+        # it will pass now
+        self.assertEqual(add(3, 7), 10)
